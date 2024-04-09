1bca4145aa1bd56208f17b70c24ad95404b5bfbf
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-09 21:33:32 +1000

    feat(task 12): add city template and route

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,96 @@
+#!/usr/bin/python3
+"""
+This module provides flask app routing certain view pages.
+"""
+from flask import Flask, render_template
+
+from models.__init__ import storage
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    return 'Hello HBNB!'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c(text):
+    return f'C {text}'
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return f'{n} is a number'
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return render_template('5-number.html', number=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    return render_template('6-number_odd_or_even.html', number=n)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states)
+
+
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states(id=None):
+    states = storage.all(State)
+    found = None
+    if id:
+        for state in states.values():
+            if state.id == id:
+                found = True
+                break
+    return render_template('9-states.html', states=states, id=id, found=found)
+
+
+@app.route('/hbnb_filters', strict_slashes=False)
+def hbnb_filters():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    return render_template(
+        '10-hbnb_filters.html',
+        states=states,
+        amenities=amenities
+    )
+
+
+@app.route('/hbnb', strict_slashes=False)
+def place():
+    states = storage.all(State)
+    amenities = storage.all(Amenity)
+    places = storage.all(Place)
+    return render_template(
+        '100-hbnb.html',
+        states=states,
+        amenities=amenities,
+        places=places
+    )
+
+
+@app.teardown_appcontext
+def teardown_app_context(exception=None):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
