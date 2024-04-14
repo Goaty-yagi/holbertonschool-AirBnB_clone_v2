3ba6232ac772be0145bfcc0ed134182e39616a83
Goaty-yagi <60104559+Goaty-yagi@users.noreply.github.com>
2024-04-14 14:35:14 +1000

    chore: add teardown_appcontext

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -16,5 +16,10 @@ def states_list():
     return render_template('7-states_list.html', states=states)
 
 
+@app.teardown_appcontext
+def teardown_app_context(exception=None):
+    storage.close()
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=False)
